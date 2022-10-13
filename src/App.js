393a78281a00172b5754acf75da2c8a865ace6e5
m8o <mattgreenesnow@gmail.com>
2022-10-13 08:55:30 +0200

    Index and Manifest name, logo edit, version displayed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PdfLoaderViewer from "./components/PdfLoaderViewer/PdfLoaderViewer";
 function App() {
   return (
     <div className="App">
+      <div className="gs_task_version">v{process.env.REACT_APP_VERSION}</div>
       <PdfLoaderViewer />
     </div>
   );
